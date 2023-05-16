a46d6d873bf25cf95ed0deed71004c7b1be7eb8b
Matej Krasić <mkrasic@foi.hr>
2023-05-16 14:25:32 +0200

    Implementiran odabir aktivnosti na formi za evaluaciju

--- Evaluation Manager (kraj 2. vježbe) (3)/Evaluation Manager/FrmEvaluation.cs
+++ Evaluation Manager (kraj 2. vježbe) (3)/Evaluation Manager/FrmEvaluation.cs
@@ -1,4 +1,5 @@
 ﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,12 +12,53 @@ using System.Windows.Forms;
 
 namespace Evaluation_Manager {
     public partial class FrmEvaluation : Form {
-        public FrmEvaluation(Student selectedStudent) {
-            InitializeComponent();
-            Student = selectedStudent;
-        }
-        
 
-      
+        private Student student;
+        private Student selectedStudent;
+
+        public FrmEvaluation(Student selectedStudent)
+        {
+            InitializeComponent();
+
+            student = selectedStudent;
+            //this.selectedStudent = selectedStudent;
+        }
+
+        public FrmEvaluation(Student selectedStudent, Student student)
+        {
+            InitializeComponent();
+            student = selectedStudent;
+            
+        }
+
+        private void FrmEvaluation_Load(object sender, EventArgs e)
+        {
+            SetFormText();
+            var activities = ActivityRepositorpy.GetActivities();
+            cboActivities.DataSource = activities;
+        }
+        private void SetFormText()
+        {
+            Text = student.FirstName + " " + student.LastName;
+        }
+
+        private void cboActivities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var currentActivity = cboActivities.SelectedItem as Activity;
+            txtActivityDescription.Text = currentActivity.Description;
+            txtMinForGrade.Text = currentActivity.MinPointsForGrade + "/" +
+           currentActivity.MaxPoints;
+            txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" +
+           currentActivity.MaxPoints;
+
+            numPoints.Minimum = 0;
+            numPoints.Maximum = currentActivity.MaxPoints;
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
